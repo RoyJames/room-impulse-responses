4122d8ef90e086739b5278774a189fdf9f5d1366
Art Pelling <a.pelling@tu-berlin.de>
2024-04-03 06:45:29 +0200

    Update MIRACLE dataset (#5)
    * update DL links
    
    * update DL script
    
    * change mode
    
    * add miracle to get_all.sh

--- get_miracle.sh
+++ get_miracle.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
+
 dest=$1
 
-echo "Download A1.h5"
-wget  --content-disposition -P $dest https://depositonce.tu-berlin.de/bitstreams/76798c39-5ced-43f4-be09-7c0c30abdd81/download
-echo "Download A2.h5"
-wget --content-disposition -P $dest https://depositonce.tu-berlin.de/bitstreams/27b81b48-24a7-4db8-acc0-5dc085c94caf/download
-echo "Download D1.h5"
-wget --content-disposition -P $dest https://depositonce.tu-berlin.de/bitstreams/f812254f-185a-4e92-aff7-9daaebfe6976/download
-echo "Download R2.h5"
-wget --content-disposition -P $dest https://depositonce.tu-berlin.de/bitstreams/2b016da5-073d-4bb1-a05d-d1337aba00a1/download
-echo "Download loudspeaker.h5"
-wget --content-disposition -P $dest https://depositonce.tu-berlin.de/bitstreams/690fbc37-b2f5-4abf-8234-cbc007062fc5/download
-echo "Finished"
+if [ "$dest" == "" ]; then
+  echo "Usage: $0 <path to store the downloaded data>"
+  exit
+fi
+
+
+mkdir -p $dest
+
+echo "Downloading A1.h5..."
+wget  --content-disposition -O $dest/A1.h5 https://depositonce.tu-berlin.de/bitstreams/67156d9c-224d-4d07-b923-be0240e7b48d/download
+echo "Downloading A2.h5..."
+wget --content-disposition -O $dest/A2.h5 https://depositonce.tu-berlin.de/bitstreams/cbb462d7-cb28-4803-98d8-84b03aad0d5f/download
+echo "Downloading D1.h5..."
+wget --content-disposition -O $dest/D1.h5 https://depositonce.tu-berlin.de/bitstreams/86680ee5-ae0c-4b38-8ef8-805652a21ded/download
+echo "Downloading R2.h5..."
+wget --content-disposition -O $dest/R2.h5 https://depositonce.tu-berlin.de/bitstreams/0fc5f5a4-a2f7-4eb7-b796-7114260e5e86/download
+echo "Downloading loudspeaker.h5..."
+wget --content-disposition -O $dest/loudspeaker.h5 https://depositonce.tu-berlin.de/bitstreams/f16367cb-14cc-44f9-bb0b-9be3c33183d3/download
